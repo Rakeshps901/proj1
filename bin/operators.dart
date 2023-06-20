1921092005cad7ddfbe0a48c83be598cc8e3b3ed
Rakeshps901 <adithyaraj012003@gmail.com>
2023-06-20 15:02:26 +0530

    initial commit

--- bin/operators.dart
+++ bin/operators.dart
@@ -1,4 +1,4 @@
-///1. Arithmetic operator
+///1. Arithmetic operator -->+,_,*,?,%,~/
 
 void main()
 {
@@ -9,4 +9,16 @@ void main()
   print("a/b=${a/b}");
   print("a%b=${a%b}");//to fetch reminder
   print("a~/b=${a~/b}");//to fetch integer result if the result is double
+
+  ///2.Assignment Operator --> =,+=,-=,*=.....etc
+
+  dynamic x=20,y=30;
+  print("x=y -->${x=y}");   //x = y   --> x=30
+  print("x+=y -->${x+=y}"); //x=x+y -->x=30 + 30=60
+  print("x-=y-->${x-=y}");  //x-=y-->x=60-30=30
+  print("x*=y -->${x*=y}"); //x*=y-->x=30*30=900
+  print("x/=y-->${x/=y}");  //x=x/y-->x900/30=30
+  //x/=y will show error since x and y are integer so div result double can't assigned to integer x
+  //so declare x and y as dynamic
+
 }
