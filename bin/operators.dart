4fd11dc33a3bb06e1ed35b00c40b0ef242769896
Rakeshps901 <adithyaraj012003@gmail.com>
2023-06-20 14:43:43 +0530

    initial commit

---
+++ bin/operators.dart
@@ -0,0 +1,12 @@
+///1. Arithmetic operator
+
+void main()
+{
+  int a=140, b=55;
+  print("a+b=${a+b}");
+  print("a-b=${a-b}");
+  print("a*b=${a*b}");
+  print("a/b=${a/b}");
+  print("a%b=${a%b}");//to fetch reminder
+  print("a~/b=${a~/b}");//to fetch integer result if the result is double
+}
