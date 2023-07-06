78627b5d27ddba7e02b0a3eb8615984d13a18a7e
Rakeshps901 <adithyaraj012003@gmail.com>
2023-07-06 14:19:48 +0530

    initial commit

---
+++ bin/collections/types of for loops.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  List<int> list=[1,2,3,4,5,6,7,8,9,10];
+
+  //1. normal for loop
+  for(int index=0;index<list.length;index++)
+    {
+      print(list[index]);
+    }
+
+  print('---------------------------------------------');
+
+  //2.for-in loop --> for(datatype-similar-to-list variablename in listname)
+  for(int i in list)
+    {
+      print(i);
+    }
+
+  print('----------------------------------------------');
+
+
+  //3.
+}
